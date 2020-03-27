ef752d777a3e373d9bb7229d7f944b8d10faf519
Chisato Murakami <kiwipom1031@gmail.com>
2020-03-27 10:26:05 -0700

    Add variables to store the count of yes

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
 $(document).ready(function(){
   // console.log($(".q1 input"))
+  var rubyCount = 0;
+  var jsCount = 0;
+  var pythonCount = 0;
+
   $(".q1-button").click(function(){
-    // console.log($(this).val())
+    if($(this).val() === yes){
+      jsCount ++;
+    }
     $('.q2').show();
   });
   $(".q2-button").click(function(){
@@ -40,6 +46,7 @@ $(document).ready(function(){
     // console.log($(this).val())
     $('.button').show();
   });
-  var numOfYes = ''
+
+  var numOfYes = $(this).val();
   
 });
