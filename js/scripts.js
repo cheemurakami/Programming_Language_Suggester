5b49b815d53aa995c0d09862dd93c7928df13151
Chisato Murakami <kiwipom1031@gmail.com>
2020-03-27 10:07:43 -0700

    Add show(); to each function to show next questions.

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+  // console.log($(".q1 input"))
+  $(".q1-button").click(function(){
+    // console.log($(this).val())
+    $('.q2').show();
+  });
+  $(".q2-button").click(function(){
+    // console.log($(this).val())
+    $('.q3').show();
+  });
+  $(".q3-button").click(function(){
+    // console.log($(this).val())
+    $('.q4').show();
+  });
+  $(".q4-button").click(function(){
+    // console.log($(this).val())
+    $('.q5').show();
+  });
+  $(".q5-button").click(function(){
+    // console.log($(this).val())
+    $('.q6').show();
+  });
+  $(".q6-button").click(function(){
+    // console.log($(this).val())
+    $('.q7').show();
+  });
+  $(".q7-button").click(function(){
+    // console.log($(this).val())
+    $('.q8').show();
+  });
+  $(".q8-button").click(function(){
+    // console.log($(this).val())
+    $('.q9').show();
+  });
+  $(".q9-button").click(function(){
+    // console.log($(this).val())
+    $('.q10').show();
+  });
+  $(".q10-button").click(function(){
+    // console.log($(this).val())
+    $('.button').show();
+  });
+  var numOfYes = ''
+  
+});
